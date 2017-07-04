a39a7366f9e745ffa5206662df4506609e869247
Sami Siren <siren@foofactory.fi>
2017-07-04 13:35:37 +0300

    push latest

--- travis-build.sh
+++ travis-build.sh
@@ -11,15 +11,20 @@ set -e
 ORG=${ORG:-hsldevcom}
 DOCKER_TAG=${TRAVIS_BUILD_ID:-latest}
 DOCKER_IMAGE=$ORG/graphiql:${DOCKER_TAG}
+LATEST_IMAGE=$ORG/graphiql:latest
+PROD_IMAGE=$ORG/graphiql:prod
+
 
 echo Building graphiql: $DOCKER_IMAGE
 
 docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ] then
+  DOCKER_IMAGE=$ORG/graphiql:${DOCKER_TAG}
   echo Pushing container: $DOCKER_IMAGE
-  docker login -u $DOCKER_USER -p $DOCKER_AUTH
-  docker push $DOCKER_IMAGE
+  docker tag ${DOCKER_IMAGE} ${LATEST_IMAGE}
+  docker login -u ${DOCKER_USER} -p ${DOCKER_AUTH}
+  docker push ${LATEST_IMAGE}
 fi
 
 echo Build completed
